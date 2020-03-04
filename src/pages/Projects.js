6aa274d5aa81c5a9c4c73c6a188c2904db3f8ef2
Gabriel Cioci <ciocigabi@gmail.com>
2020-03-04 20:45:46 +0200

    added projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,6 +5,7 @@ import '../SCSS/Projects.scss';
 import slots from '../assets/slot-machine.png';
 import lightsout from '../assets/lightsout.png';
 import pathfinding from '../assets/pathfinding.png';
+import quotes from '../assets/quotes.png';
 
 class Projects extends Component {
 	state = {};
@@ -34,12 +35,20 @@ class Projects extends Component {
 						/>
 						<SingleProject
 							className="col-sm-12 col-md-6 col-lg-4 single-project"
+							title="Random Quote"
+							featureImage={quotes}
+							tech="ReactJS, HTML, SASS, API"
+							github="https://github.com/gabrielcioci/random-quotes"
+							demo="https://gabrielcioci.github.io/random-quotes/"
+						/>
+						{/* <SingleProject
+							className="col-sm-12 col-md-6 col-lg-4 single-project"
 							title="Pathfinding A*"
 							featureImage={pathfinding}
 							tech="HTML, P5, CSS"
 							github="https://github.com"
 							demo="https://google.ro"
-						/>
+						/> */}
 					</div>
 				</div>
 			</div>
